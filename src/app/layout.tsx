d361a8e2cef461c3d5a72425ec64850134007fd1
Mark Stockhausen <mail@markstockhausen.com>
2026-01-07 09:46:46 +0100

    chore: update configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en" className={inter.variable}>
+        <html lang="en" className={inter.variable} suppressHydrationWarning>
         <body className="font-sans">
         <Navbar />
         {children}
